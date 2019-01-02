8287dd556272a8c807d09804f05af1ae652648dc
tumbling-dice <inujini.dev@gmail.com>
2019-01-03 00:04:31 +0900

    :sushi: ステートをクラス化

---
+++ src/store/modules/search/models.js
@@ -0,0 +1,18 @@
+/**
+ * 検索結果
+ */
+class SearchResult {
+  /**
+   * 検索結果
+   */
+  constructor() {
+    /** ID */
+    this.id = "";
+    /** 名前 */
+    this.name = "";
+  }
+}
+
+export {
+  SearchResult
+}
--- src/store/modules/search/state.js
+++ src/store/modules/search/state.js
@@ -1,5 +1,23 @@
-const state = {
-  result: []
+import * as Models from './models';
+
+/**
+ * 検索用ステート
+ */
+class SearchState {
+  /**
+   * 検索用ステート
+   */
+  constructor() {
+    /**
+     * 検索結果
+     * @type {Models.SearchResult[]}
+     */
+    this.result = [];
+  }
+}
+
+export {
+  SearchState
 };
 
-export default state;
+export default new SearchState();
