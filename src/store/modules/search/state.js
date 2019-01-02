279266ad32853cc807595f478abbc67eda7b2928
tumbling-dice <inujini.dev@gmail.com>
2019-01-03 01:21:00 +0900

    :sushi: 適当なゲッターを定義

--- src/store/modules/search/state.js
+++ src/store/modules/search/state.js
@@ -14,6 +14,22 @@ class SearchState {
      */
     this.result = [];
   }
+
+  // これをゲッターの代わりにする
+  get sortedByIdResult() {
+    return this.result.sort((a, b) => {
+      const idA = a.id.toUpperCase();
+      const idB = b.id.toUpperCase();
+      if (idA < idB) {
+        return -1;
+      }
+      if (idA > idB) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
 }
 
 export {
